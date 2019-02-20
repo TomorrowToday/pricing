f3b57929c13b3edc5d0fd169a65afca55bdcde89
Steven Lischer <xcpostman06@gmail.com>
2019-02-20 11:18:47 -0500

    initial setup of requirements

---
+++ pricing.py
@@ -0,0 +1,35 @@
+import pickle
+import os.path
+import requests
+
+from bs4 import BeautifulSoup
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from selenium import webdriver
+
+
+
+def main():
+
+    # get price sheet data
+
+    # format price sheet data
+
+    # connect to cart
+
+    # for country in price sheet
+
+        # get cart for country
+
+        # for option in cart
+
+            # format cart option data
+
+
+    # find differences between price sheet and cart
+
+    # display/report differnces between price sheet and cart
+
+if __name__ == '__main__':
+    main()
